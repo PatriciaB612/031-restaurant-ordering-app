5b712e53f6798b7c77410bd877cd9db4810b20c2
Silvia Patricia Soto <122170353+PatriciaB612@users.noreply.github.com>
2025-03-03 16:53:05 -0400

    Added functionality to account for multiple item occurrences
    Added functionality to account for multiple item occurrences.
    Renamed variables for better clarity.

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 import { menuArray } from './data.js'
-const menu = document.querySelector('.menu')
-const orderArray = []
-const orderItems = document.querySelector('.order-items')
+
+const menuEl = document.querySelector('.menu')
 const orderDiv = document.querySelector('.order')
-const totalPriceDiv = document.querySelector('.total-price')
-const modal = document.querySelector('.modal')
+const orderItemsDiv = document.querySelector('.order-items')
+const totalPriceEl = document.querySelector('.total-price')
+const modalEl = document.querySelector('.modal')
 const paymentForm = document.querySelector('form')
+const thankYouMessageEl = document.querySelector('.thank-you-message')
+let orderArray = []
 
 function renderMenuItems() {
-  menuArray.forEach(function (menuItem) {
-    menu.innerHTML += `
+  menuArray.forEach(function (item) {
+    menuEl.innerHTML += `
         <div class="item">
           <div class="emoji-container">
-            <div class="item-emoji">${menuItem.emoji}</div>
+            <div class="item-emoji">${item.emoji}</div>
           </div>
           <div class="item-description">
-            <p class="item-name">${menuItem.name}</p>
-            <p class="item-ingredients">${menuItem.ingredients}</p>
-            <p class="item-price">${'$' + menuItem.price}</p>
+            <p class="item-name">${item.name}</p>
+            <p class="item-ingredients">${item.ingredients}</p>
+            <p class="item-price">${'$' + item.price}</p>
           </div>
           <div class="add-btn-container">
             <button class="add-btn btn">
-              <img src="images/add-btn.png" data-add="${menuItem.id}">
+              <img src="images/add-btn.png" data-add="${item.id}">
             </button>
           </div>
         </div>
@@ -32,77 +34,78 @@ renderMenuItems()
 
 document.addEventListener('click', function (e) {
   if (e.target.dataset.add) {
-    handleAddClick(e.target.dataset.add)
+    handleAddBtnClick(e.target.dataset.add)
     orderDiv.style.display = 'revert'
-    renderOrder()
-    renderTotalPrice()
   } else if (e.target.dataset.remove) {
-    removeItem(e.target)
-    renderOrder()
-    renderTotalPrice()
+    handleRemoveBtnClick(e.target)
   } else if (e.target.id === 'complete-btn') {
     handleCompleteBtnClick()
-  } else if (e.target.id === 'pay-btn') {
-    handlePayBtnClick()
   }
 })
 
-function handleAddClick(itemId) {
-  if (menuArray[itemId]) {
-    orderArray.push(menuArray[itemId])
-    return orderArray
-  }
+function handleAddBtnClick(itemId) {
+  orderArray.push(menuArray[itemId])
+  orderItemsDiv.innerHTML = ''
+  renderOrder()
+  thankYouMessageEl.innerHTML = ''
 }
 
 function renderOrder() {
-  orderItems.innerHTML = ''
-  orderArray.forEach(function (orderItem) {
-    orderItems.innerHTML += `
+  const count = []
+
+  orderArray.forEach((item) => {
+    count[item.name] = (count[item.name] || 0) + 1
+  })
+  const uniqueItemsArray = orderArray.filter((item, index, self) => {
+    return self.indexOf(item) === index
+  })
+  uniqueItemsArray.forEach((item) => {
+    orderItemsDiv.innerHTML += `
   <div class="order-item">
-    <p class="item-name-order item-name">${orderItem.name}</p>
+    <p class="item-name-order item-name">${count[item.name]} x ${item.name}</p>
     <button class="remove-btn btn" data-remove="${orderArray.indexOf(
-      orderItem
+      item
     )}">remove</button>
-    <p class="item-price-order item-price">${'$' + orderItem.price}</p>
+    <p class="item-price-order item-price">${
+      '$' + count[item.name] * item.price
+    }</p>
   </div>
   `
   })
+  renderTotalPrice()
 }
 
 function renderTotalPrice() {
   let totalPrice = 0
-  for (let order of orderArray) {
-    totalPrice += order.price
+  for (let item of orderArray) {
+    totalPrice += item.price
   }
-  totalPriceDiv.innerHTML = `${'$' + totalPrice}`
+  totalPriceEl.innerHTML = `${'$' + totalPrice}`
 }
 
-function removeItem(eventTarget) {
+function handleRemoveBtnClick(eventTarget) {
+  orderItemsDiv.innerHTML = ''
   const itemIndex = eventTarget.dataset.remove
   orderArray.splice(itemIndex, 1)
+  renderOrder()
 }
 
 function handleCompleteBtnClick() {
-  modal.style.display = 'block'
+  modalEl.style.display = 'block'
+  orderArray = []
 }
-
-function handlePayBtnClick() {}
 
 paymentForm.addEventListener('submit', function (e) {
   e.preventDefault()
   const paymentFormData = new FormData(paymentForm)
   const cardholderName = paymentFormData.get('cardholderName')
 
-  console.log(paymentFormData)
-  console.log(cardholderName)
-
-  modal.style.display = 'none'
+  modalEl.style.display = 'none'
   orderDiv.style.display = 'none'
-  totalPriceDiv.style.display = 'none'
+  totalPriceEl.style.display = 'none'
 
-  const thankYouMessage = document.querySelector('.thank-you-message')
-  thankYouMessage.style.display = 'block'
-  thankYouMessage.innerHTML = `
+  thankYouMessageEl.style.display = 'block'
+  thankYouMessageEl.innerHTML = `
      <p>Thanks ${cardholderName}! Your order is on its way!</p>
      `
 })
